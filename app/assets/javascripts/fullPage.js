bd725e486d1863383cfc8263539b4960766fbb31
Sechin Andrey <andreyka12@mail.ru>
2016-03-15 22:19:21 +0200

    add slide

--- app/assets/javascripts/fullPage.js
+++ app/assets/javascripts/fullPage.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('.fullpage').fullpage({
         menu: '.navbar',
-        scrollingSpeed: 10,
+        scrollingSpeed: 500,
         verticalCentered: true,
         resize : false,
-        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        navigation: true,
+        anchors:['firstPage', 'secondPage', 'thirdPage'],
+        navigation: false,
         navigationPosition: 'right',
         navigationTooltips:['firstPageTooltip', 'secondPageTooltip', 'thirdPageTooltip', 'fourthPageTooltip'],
-        css3: true
+        css3: true,
     });
 });
