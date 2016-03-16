9449dfbbf311b2ca03a514508cf7749b9725fe64
Sechin Andrey <andreyka12@mail.ru>
2016-03-16 19:34:06 +0200

    add marker.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,49 @@
-var map;
+
+var myLatLng = {lat: 47.860803, lng: 35.0992256};
+
 function initMap() {
-    new google.maps.Map($('#map')[0], {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+    map = new google.maps.Map($('#map')[0], {
+        center: myLatLng,
+        zoom: 10
+    });
+    marker =  new google.maps.Marker({
+        position: myLatLng,
+        map: map
     });
 }
 
-$(document).ready(function() {
-    map = initMap();
-});
+//OLD CODE
+
+//var map;
+//var marker;
+
+//
+//function initMap() {
+//    return new google.maps.Map($('#map')[0], {
+//        center: myLatLng,
+//        zoom: 10
+//    });
+//}
+//
+//function initMarker(map) {
+//    return new google.maps.Marker({
+//        position: myLatLng,
+//        map: map
+//    });
+//}
+//
+//
+//$(document).ready(function() {
+//    map = new google.maps.Map($('#map')[0], {
+//        center: myLatLng,
+//        zoom: 10
+//    });
+//
+//    marker =  new google.maps.Marker({
+//        position: myLatLng,
+//        map: map
+//    });
+//    //map = initMap();
+//    //marker = initMarker(map);
+//});
+
