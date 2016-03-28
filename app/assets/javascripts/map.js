2abacb8c068ce0e7a88960eedb5f4b3ac56a7f0f
Sechin Andrey <andreyka12@mail.ru>
2016-03-28 21:35:11 +0300

    add marker.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,28 +1,50 @@
-
 var myLatLng = {lat: 47.860803, lng: 35.0992256};
+var map;
+var origin_marker;
+var destination_marker;
+
 
 function initMap() {
     map = new google.maps.Map($('#map')[0], {
         center: myLatLng,
         zoom: 10
     });
-    marker =  new google.maps.Marker({
-        position: myLatLng,
-        map: map
-    });
+    //my_marker =  new google.maps.Marker({
+    //    position: myLatLng,
+    //    map: map
+    //});
 
     google.maps.event.addListener(map, 'click', function(event) {
-        placeMarker(event.latLng);
+        if ($("#origin").is(":focus")) {
+            origin_marker = placeMarker(origin_marker, event.latLng, 'A');
+            console.log(event.latLng)
+        }else if($("#destination").is(":focus")){
+            destination_marker = placeMarker(destination_marker, event.latLng, 'B');
+        }
+
     });
-}
 
-
-function placeMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
+    var directionsDisplay = new google.maps.DirectionsRenderer({
         map: map
     });
+    
 }
+
+function placeMarker(marker, location, lable) {
+
+    if (marker == undefined) {
+    } else {
+        marker.setMap(null);
+    }
+
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        label: lable
+    });
+    return marker;
+}
+
 
 //OLD CODE
 
