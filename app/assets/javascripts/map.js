5311da40086f34bd8ffb37dee8093a49955dcabe
SechinAndrey <andreyka12@mail.ru>
2016-03-29 04:29:29 +0300

    Google Maps Directions, Matrix API, Time Zone API, Map Style.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,12 +3,44 @@ var map;
 var origin_marker;
 var destination_marker;
 
-
 function initMap() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
     map = new google.maps.Map($('#map')[0], {
         center: myLatLng,
         zoom: 10
     });
+
+	var styles = [
+  {
+    stylers: [
+      { hue: "#00ffe6" },
+      { saturation: -20 }
+    ]
+  },{
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      { lightness: 100 },
+      { visibility: "simplified" }
+    ]
+  },{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+map.setOptions({styles: styles});
+
+    directionsDisplay.setMap(map);
+
+    var service = new google.maps.DistanceMatrixService();
+
+
     //my_marker =  new google.maps.Marker({
     //    position: myLatLng,
     //    map: map
@@ -17,17 +49,44 @@ function initMap() {
     google.maps.event.addListener(map, 'click', function(event) {
         if ($("#origin").is(":focus")) {
             origin_marker = placeMarker(origin_marker, event.latLng, 'A');
-            console.log(event.latLng)
+            $("#origin").val(event.latLng)
         }else if($("#destination").is(":focus")){
             destination_marker = placeMarker(destination_marker, event.latLng, 'B');
+            $("#destination").val(event.latLng)
         }
-
     });
 
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-        map: map
+    $('#get_directions').click(function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        calculateDistance(service);
+
+        console.log(origin_marker.position);
+
+        $.ajax({
+            //test location
+            url:"https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp="+(Math.round((new Date().getTime())/1000)).toString()+"&sensor=false"
+        }).done(function(response){
+            if(response.timeZoneId != null){
+               console.log(response.timeZoneId);
+            }
+        });
     });
+    //var directionsDisplay = new google.maps.DirectionsRenderer({
+    //    map: map
+    //});
     
+}
+
+function callback(response, status) {
+
+
+    if (status == "OK") {
+        console.log(response.destinationAddresses[0]);
+        console.log(response.originAddresses[0]);
+        console.log(response.rows[0].elements[0].distance.text);
+    } else {
+        alert("Error: " + status);
+    }
 }
 
 function placeMarker(marker, location, lable) {
@@ -44,6 +103,35 @@ function placeMarker(marker, location, lable) {
     });
     return marker;
 }
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    console.log($("#origin").val());
+    directionsService.route({
+        origin: origin_marker.position,
+        destination: destination_marker.position,
+        travelMode: google.maps.TravelMode.WALKING
+    }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            origin_marker.setMap(null);
+            destination_marker.setMap(null);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+
+function calculateDistance(service){
+    service.getDistanceMatrix(
+        {
+            origins: [origin_marker.position],
+            destinations: [destination_marker.position],
+            travelMode: google.maps.TravelMode.DRIVING,
+            avoidHighways: false,
+            avoidTolls: false
+        }, callback);
+}
+
 
 
 //OLD CODE
